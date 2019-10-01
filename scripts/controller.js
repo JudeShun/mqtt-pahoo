ac3c14643b668551892dc9232582804096084bf2
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-10-01 16:19:53 +0800

    final

---
+++ scripts/controller.js
@@ -0,0 +1,137 @@
+// basic functionalities
+
+//broker
+var btnConnect = document.getElementById('connect');
+var btnDisConnect = document.getElementById('disconnect');
+var broker = document.getElementById('broker');
+var btnStatus = document.getElementById('status');
+function MyConnect() {
+  alert("You are now successfully connected in MQTT!");
+}
+function MyDisconnect() {
+  alert("You are now successfully disconnected from MQTT!");
+}
+
+//publisher
+var btnPublish = document.getElementById('btnPublish');
+var pubTopic = document.getElementById('pubTopic');
+var pubPayload = document.getElementById('pubPayload');
+
+//subscriber
+var subTopic = document.getElementById('subTopic');
+var btnSubscribe = document.getElementById('btnSubscribe');
+var btnUnsubscribe = document.getElementById('btnUnsubscribe');
+
+
+//btnConnect
+btnConnect.addEventListener('click', function (e) {
+  e.preventDefault();
+  //client
+  var client = mqtt.connect(broker.value)
+  // client.subscribe("mqtt/demox")
+
+  btnSubscribe.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log(subTopic.value)
+    client.subscribe(subTopic.value);
+    btnUnsubscribe.disabled = false;
+    btnSubscribe.disabled = true;
+  })
+
+  btnUnsubscribe.addEventListener('click', function (e) {
+    e.preventDefault();
+    client.unsubscribe(subTopic.value);
+    btnUnsubscribe.disabled = true;
+    btnSubscribe.disabled = false;
+    console.log("Unsubscribe to " + subTopic.value)
+  })
+
+  client.on("connect", function () {
+    console.log("You are successfully connected");
+    btnStatus.disabled = false;
+    btnDisConnect.disabled = false;
+    btnConnect.disabled = true;
+    btnStatus.setAttribute('value', 'You are successfully Connected!')
+    btnStatus.setAttribute('class', 'btn btn-success')
+  });
+
+
+  //btnDisconnect
+  btnDisConnect.addEventListener('click', function () {
+    client.end();
+    btnStatus.disabled = true;
+    btnDisConnect.disabled = true;
+    btnConnect.disabled = false;
+    console.log('Disconnected');
+    btnStatus.setAttribute('value', 'Successfully Disconnected!')
+    btnStatus.setAttribute('class', 'btn btn-warning')
+  });
+
+
+  client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+    let tbl = document.getElementById('receiver');
+    let tbody = document.getElementById('msg');
+    let tr = document.createElement('tr');
+    let msgTopic = document.createElement('td');
+    let msgPayload = document.createElement('td');
+    let msgTime = document.createElement('td');
+    msgTopic.appendChild(document.createTextNode(topic));
+    msgPayload.appendChild(document.createTextNode(payload));
+    msgTime.appendChild(document.createTextNode(moment().format('llll')));
+    tr.appendChild(msgTopic);
+    tr.appendChild(msgPayload);
+    tr.appendChild(msgTime);
+    tbody.appendChild(tr);
+    tbl.appendChild(tbody);
+    // $('.broker tbody').append("<tr><td>" + finalTopic + "</td><td>" + payload + "</td><td>" + moment().format('llll') + "</td></tr>");
+
+  })
+
+  // client.publish("mqtt/demox", "hello world!")
+
+  btnPublish.addEventListener('click', function (e) {
+    e.preventDefault();
+    client.publish(pubTopic.value, pubPayload.value)
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // advance functionalities
+// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+// client.subscribe("mqtt/demo", function (err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("subscribed")
+//   }
+// })
+
+// client.on("connect", function(){
+//     console.log("Successfully connected");
+// })
+
+// client.on("message", function (topic, payload) {
+//   console.log([topic, payload].join(": "));
+//   client.end();
+// })
+
+// client.publish("mqtt/demo", "hello world!", function(err){
+//   if (err){
+//     console.log(err)
+//   } else {
+//     console.log("published")
+//   }
+// })
